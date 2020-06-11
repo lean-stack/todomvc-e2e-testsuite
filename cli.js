d7d4d14a480c00efa084746e6f2187324db26634
Micha Alt <mail@lean-stack.de>
2020-06-11 13:03:12 +0200

    Added bin script.

---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const cypress = require('cypress');
+const { help } = require('yargs');
+
+require('yargs')
+  .scriptName("todomvc-e2e-cy-testsuite")
+
+  .command('headless', 'start a headless testsuite', {}, argv => {
+    cypress.run({
+      config: {
+        baseUrl: `http://localhost:${argv.port || 4200}`
+      }
+    });
+  })
+  .command('ui', 'start a ui testsuite', {}, (argv) => {
+    cypress.open({
+      config: {
+        baseUrl: `http://localhost:${argv.port || 4200}`
+      }
+    });
+  })
+  .demandCommand(1,
+    'You need a command (ui or headless) before moving on',
+  )
+  .option('port', {
+    alias: 'p',
+    number: true,
+    default: 4200,
+    description: 'port the app to test is listening on'
+  })
+  .argv;
